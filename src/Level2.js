6a38e8e2008f48efd780f9341d90f4f583500d3d
Jaaron-Gunpot <jsw6194@rit.edu>
2024-11-02 21:45:22 -0800

    edited difficulty

--- src/Level2.js
+++ src/Level2.js
@@ -27,16 +27,51 @@ class Level2 extends BaseScene {
         // Add thorns
         this.thorns1 = this.add.image(300, 450, 'thorns').setScale(0.25);
         this.thorns2 = this.add.image(850, 450, 'thorns').setScale(0.25);
+        this.thorns3 = this.add.image(1400, 450, 'thorns').setScale(0.25);
+        this.thorns4 = this.add.image(2900, 450, 'thorns').setScale(0.25);
+        this.thorns5 = this.add.image(3400, 450, 'thorns').setScale(0.25);
 
         this.physics.add.existing(this.thorns1, true);
         this.physics.add.existing(this.thorns2, true);
+        this.physics.add.existing(this.thorns3, true);
+        this.physics.add.existing(this.thorns4, true);
+        this.physics.add.existing(this.thorns5, true);
 
         // Enable collision detection between player and thorns
         this.physics.add.collider(this.player, this.thorns1, super.playerHIt, null, this);
         this.physics.add.collider(this.player, this.thorns2, super.playerHIt, null, this);
+        this.physics.add.collider(this.player, this.thorns3, super.playerHIt, null, this);
+        this.physics.add.collider(this.player, this.thorns4, super.playerHIt, null, this);
+        this.physics.add.collider(this.player, this.thorns5, super.playerHIt, null, this);
+
+
+        //set up platforms
+        this.platform1 = this.add.rectangle(1500, 500, 500, 50, 0x00ff00);
+        this.physics.add.existing(this.platform1, true); // true makes it static X increments by 750
+        this.physics.add.collider(this.player, this.platform1);
+
+        this.platform2 = this.add.rectangle(2500, 500, 500, 50, 0x00ff00);
+        this.physics.add.existing(this.platform2, true); // true makes it static
+        this.physics.add.collider(this.player, this.platform2);
+
+        this.platform3 = this.add.rectangle(3000, 500, 250, 50, 0x00ff00);
+        this.physics.add.existing(this.platform3, true); // true makes it static
+        this.physics.add.collider(this.player, this.platform3);
+
+        this.platform4 = this.add.rectangle(4750, 500, 500, 50, 0x00ff00);
+        this.physics.add.existing(this.platform4, true); // true makes it static
+        this.physics.add.collider(this.player, this.platform4);
+
+        this.platform5 = this.add.rectangle(5500, 500, 500, 50, 0x00ff00);
+        this.physics.add.existing(this.platform5, true); // true makes it static
+        this.physics.add.collider(this.player, this.platform5);
+
+        this.platform6 = this.add.rectangle(6250, 500, 500, 50, 0x00ff00);
+        this.physics.add.existing(this.platform6, true); // true makes it static
+        this.physics.add.collider(this.player, this.platform6);
 
         // Create a goal specific to Level 2
-        this.goal = this.add.rectangle(1200, 450, 50, 50, 0x0000ff);
+        this.goal = this.add.rectangle(6500, 450, 50, 50, 0x0000ff);
         this.physics.add.existing(this.goal, true);
 
         // Enable collision detection between player and goal
@@ -45,14 +80,14 @@ class Level2 extends BaseScene {
         this.opening();
 
         // Add Enemy
-        this.addEnemy(1000, 100, 'raccoon', -10);
+        // this.addEnemy(1000, 100, 'raccoon', -10);
 
-        this.enemies.children.iterate((enemy) => {
-            enemy.body.setSize(enemy.width * 0.6, enemy.height * 0.52);
-            enemy.body.setOffset(enemy.width * 0.25, enemy.height * 0.25);
-            enemy.setCollideWorldBounds(true);
-            enemy.setScale(0.5);
-        });
+        // this.enemies.children.iterate((enemy) => {
+        //     enemy.body.setSize(enemy.width * 0.6, enemy.height * 0.52);
+        //     enemy.body.setOffset(enemy.width * 0.25, enemy.height * 0.25);
+        //     enemy.setCollideWorldBounds(true);
+        //     enemy.setScale(0.5);
+        // });
     }
 
     opening() {
